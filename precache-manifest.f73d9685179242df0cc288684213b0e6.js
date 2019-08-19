3f4b9be2f19efa889ceb824349c7e420cdd20a92
baseon2016 <baseon_000@163.com>
2019-08-19 16:07:27 +0800

    second time update gh-pages

---
+++ precache-manifest.f73d9685179242df0cc288684213b0e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876fcd22f83ce282b0048ee77c5a3f58",
+    "url": "/react-bushu/index.html"
+  },
+  {
+    "revision": "e325d62729425733d68c",
+    "url": "/react-bushu/static/css/main.b689435b.chunk.css"
+  },
+  {
+    "revision": "0681e5bb692bcc9416aa",
+    "url": "/react-bushu/static/js/2.ed64165c.chunk.js"
+  },
+  {
+    "revision": "e325d62729425733d68c",
+    "url": "/react-bushu/static/js/main.15caa268.chunk.js"
+  },
+  {
+    "revision": "b1ea01805cda07faae84",
+    "url": "/react-bushu/static/js/runtime~main.d25e4b4b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.824f175b559101535aeb8c5cfa5043f2.js"
+  "/react-bushu/precache-manifest.f73d9685179242df0cc288684213b0e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-bushu/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
